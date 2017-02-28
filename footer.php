e52e9a2b9bec4fd9ae6c2bcad1edcc5edb3c89db
josecaos <jazzvoon@gmail.com>
2017-02-28 01:04:12 -0600

    esqueleto footer widgets

--- footer.php
+++ footer.php
@@ -3,13 +3,40 @@
 </div><!-- main -->
 
 
+<footer id="footer" class="columns p-0 h-a">
 
-    <script src="bower_components/jquery/dist/jquery.js"></script>
-    <script src="bower_components/what-input/dist/what-input.js"></script>
-    <script src="bower_components/foundation-sites/dist/js/foundation.js"></script>
-    <script src="bower_components/slick-carousel/slick/slick.js"></script>
-    <script src="bower_components/imgLiquid/js/imgLiquid.js"></script>
-    <script src="submodulos/js_utils/dist/js/js_utils.js"></script>
-    <script src="js/app.js"></script>
-  </body>
+<div class="columns small-12 medium-4 h-50-v h-footer-desk v-center">
+
+<div class="columns h-a text-center">
+  Widget contacto
+</div>
+
+</div>
+<div class="columns small-12 medium-4 h-50-v h-footer-desk v-center">
+
+<div class="columns h-a text-center">
+  Widget texto
+</div>
+
+</div>
+<div class="columns small-12 medium-4 h-50-v h-footer-desk v-center">
+
+<div class="columns h-a text-center">
+  Widget banners
+</div>
+
+</div>
+
+</footer>
+
+
+
+<script src="bower_components/jquery/dist/jquery.js"></script>
+<script src="bower_components/what-input/dist/what-input.js"></script>
+<script src="bower_components/foundation-sites/dist/js/foundation.js"></script>
+<script src="bower_components/slick-carousel/slick/slick.js"></script>
+<script src="bower_components/imgLiquid/js/imgLiquid.js"></script>
+<script src="submodulos/js_utils/dist/js/js_utils.js"></script>
+<script src="js/app.js"></script>
+</body>
 </html>
