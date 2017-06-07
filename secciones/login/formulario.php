7c1a2e5b3bc6ab3a1fe37d220a1ae989bbf4eb5c
josecaos <jazzvoon@gmail.com>
2017-06-07 12:11:24 -0500

    Warning salida de sesion

--- secciones/login/formulario.php
+++ secciones/login/formulario.php
@@ -1,18 +1,24 @@
 <?php
 if ( ! is_user_logged_in() ) { // Display WordPress login form:
-    $args = array(
-        'redirect' => admin_url(),
-        'form_id' => 'loginform-custom',
-        'label_username' => __( 'Nombre de usuario' ),
-        'label_password' => __( 'Contraseña' ),
-        'label_remember' => __( 'Recuérdame' ),
-        'label_log_in' => __( 'Inicia' ),
-        'remember' => false
-    );
-    wp_login_form( $args );
+  $args = array(
+    'redirect' => admin_url(),
+    'form_id' => 'loginform-custom',
+    'label_username' => __( 'Nombre de usuario' ),
+    'label_password' => __( 'Contraseña' ),
+    'label_remember' => __( 'Recuérdame' ),
+    'label_log_in' => __( 'Inicia' ),
+    'remember' => false
+  );
+  wp_login_form( $args );
 } else { // If logged in:
-    wp_loginout( home_url() ); // Display "Log Out" link.
-    // sin registro en el front
-    // wp_register('', '');
+
+  ?>
+  <div class="columns p-1 text-center font-l">
+
+    Saliendo de la sesión ...
+
+  </div>
+  <?php
+
 }
 ?>
