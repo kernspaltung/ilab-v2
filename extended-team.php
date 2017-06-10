ea3e16a950e9411e4a2d8f3419fc2e42495d5585
josecaos <jazzvoon@gmail.com>
2017-06-09 22:32:54 -0500

    Agrega plantilla extended-team + single

--- extended-team.php
+++ extended-team.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Staff
+Template Name: Extended Team
 */
 get_header();
 ?>
@@ -23,7 +23,7 @@ get_header();
       while (have_posts()): the_post();
       ?>
       <!--  -->
-      <div id="encabezado-perfil" class="columns small-12 p-1 h-a end">
+      <div id="encabezado-mentores" class="columns small-12 p-1 h-a end">
 
         <h4 class="column p-0 s text-left font-light">
           <?php
@@ -48,16 +48,15 @@ get_header();
 
   <!-- inicia perfil  -->
   <?php
-  $args = cpt('staff');
+  $args = cpt('extended-team');
   $q = new WP_Query( $args );
 
   if ( $q->have_posts()):
     while ( $q->have_posts()):  $q->the_post();
     ?>
-    
-    <div class="columns small-12 medium-6 large-4 text-center font-m font-md-s">
+    <div class="columns small-12 medium-6 large-4 text-center  font-m font-md-s">
 
-      <a href="<?php echo  get_the_permalink();?>" class="columns p-0 h-a card">
+      <a href="<?php echo get_the_permalink();?>" class="columns p-0 h-a card">
 
         <div class="w-100 p-2 h-40-v">
           <div class="columns round p-0 imgLiquid imgLiquidFill">
@@ -69,10 +68,10 @@ get_header();
         <div class=" texto-perfil columns p-0-3 h-a">
 
           <div class="columns h-a font-light">
-            <?php echo get_post_meta(get_the_ID(),"staff-nombre",true); ?>
+            <?php echo get_post_meta(get_the_ID(),"extended-nombre",true); ?>
           </div>
           <div class="columns h-a">
-            <?php echo get_post_meta(get_the_ID(),"staff-puesto",true); ?>
+            <?php echo get_post_meta(get_the_ID(),"extended-area",true); ?>
           </div>
 
         </div>
---
+++ single-extended-team.php
@@ -0,0 +1,80 @@
+<?php
+get_header();
+
+?>
+
+<section id="content" class="content columns small-12 large-9">
+
+  <div class="columns p-1">
+
+    <!--  -->
+    <div class="bread columns p-0-2 h-10">
+      <?php
+      get_template_part('secciones/general/breadcrumbs')
+      ?>
+    </div>
+    <!--  -->
+
+
+      <?php
+
+      if ( have_posts() ):
+        while ( have_posts() ): the_post();
+
+
+        ?>
+
+        <div class="columns p-t-2 p-b-2 h-80 h-md-70 v-center">
+
+
+          <div class="columns small-12 medium-6 large-4 p-0 h-40-v m-b-2 h-40 imgLiquid imgLiquidFill h-45-v h-md-40-v">
+            <?php echo get_the_post_thumbnail(); ?>
+          </div>
+
+          <div class="columns small-12 medium-6 large-8 h-15-v m-b-2 h-md-40-v rel font-m font-lg-l">
+
+            <div class="columns p-0 h-a p-l-1 absDownL">
+
+              <div class="columns p-0 h-a font-light">
+                <label for="">Nombre: </label>
+                <?php
+                  echo get_post_meta(get_the_ID(),"extended-nombre",true);
+                ?>
+              </div>
+              <div class="columns p-0 h-a font-light">
+                <label for="">Tema: </label>
+                <?php
+                  echo get_post_meta(get_the_ID(),"extended-area",true);
+                ?>
+              </div>
+
+            </div>
+
+          </div>
+          <hr>
+          <div class="columns p-0 p-t-1 h-a font-s font-sm-m font-md-s">
+            <label for="">Biografía: </label>
+            <?php
+              echo get_post_meta(get_the_ID(),"extended-bio",true);
+            ?>
+          </div>
+
+
+
+        </div>
+
+
+        <?php
+
+      endwhile;
+    endif;
+    ?>
+
+
+
+</div>
+
+</section>
+
+<?php
+get_footer();
