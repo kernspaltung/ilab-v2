ecd653db19af604fcc23dc3f7622ab4f1ce5f7f3
josecaos <jazzvoon@gmail.com>
2017-06-30 14:38:04 -0500

    Plantilla de archivo + corrreccion a secciones eventos + ac

--- secciones/inicio/eventos-ac.php
+++ secciones/inicio/eventos-ac.php
@@ -1,4 +1,4 @@
-<section id="inicio-eventos-ac" class="section columns p-top fp-auto-height">
+<section id="inicio-eventos-ac" class="section columns p-top p-b-1 fp-auto-height">
 
   <!--  -->
   <?php
@@ -12,16 +12,16 @@
 
     <div class="columns h-35-v small-6">
 
-    <a href="<?php echo get_permalink();?>" class="columns h-100">
+      <a href="<?php echo get_permalink();?>" class="columns h-80">
 
         <div class="eventos-ac columns p-0 imgLiquid imgLiquidNoFill">
           <img src="<?php echo get_stylesheet_directory_uri();?>/img/eventos-color.png" alt="Eventos" />
         </div>
         <!-- <h1 class="columns p-0 text-center h-a texto-gigante">E</h1> -->
-
       </a>
+      <h4 class="columns text-center h-20">Eventos</h4>
 
-      </div>
+    </div>
     <?php
   endwhile;
 endif;
@@ -38,8 +38,8 @@ if ( $query->have_posts() ) :
   while ( $query->have_posts() ) : $query->the_post();
   ?>
 
-    <div class="columns h-35-v small-6">
-      <a href="<?php echo get_permalink();?>" class="columns h-100">
+  <div class="columns h-35-v small-6">
+    <a href="<?php echo get_permalink();?>" class="columns h-80">
 
       <!-- <h1 class="columns p-0 text-center h-a texto-gigante">AC</h1> -->
       <div class="eventos-ac columns p-0 imgLiquid imgLiquidNoFill">
@@ -47,7 +47,8 @@ if ( $query->have_posts() ) :
       </div>
 
     </a>
-    </div>
+    <h4 class="columns text-center h-20">AC</h4>
+  </div>
   <?php
 endwhile;
 endif;
